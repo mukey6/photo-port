9b85a8bd444a283e03d3cd6f9198151864dbf776
muktar ahmed <muktar.ahmed13@yahoo.com>
2022-02-23 22:39:17 -0600

    updated contact section and rendering pages

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState("");
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    // setFormState({...formState, name: e.target.value })
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+    // find out tge why spread was used here??
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // why e prevent??
+    console.log(formState);
+  }
+
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            defaultValue={name}
+            onChange={handleChange}
+            name="name"
+          />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            defaultValue={email}
+            name="email"
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea
+            name="message"
+            defaultValue={message}
+            onChange={handleChange}
+            rows="5"
+          />
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
         </div>
+
         <button type="submit">Submit</button>
       </form>
     </section>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -56,9 +56,7 @@
 
 // export default Nav;
 
-
-
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
@@ -66,16 +64,26 @@ function Nav(props) {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected,
   } = props;
   useEffect(() => {
     document.title = capitalizeFirstLetter(currentCategory.name);
   }, [currentCategory]);
-  
+
   return (
     <header className="flex-row px-1">
       <h2>
-        <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        <a
+          data-testid="about"
+          href="#about"
+          onClick={() => setContactSelected(false)}
+        >
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
         </a>
       </h2>
       <nav>
@@ -85,17 +93,20 @@ function Nav(props) {
               About me
             </a>
           </li>
-          <li className="mx-2">
-            <span>Contact</span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
-            <li
-              className={`mx-1 ${ currentCategory.name === category.name && 'navActive' }`}
-              key={category.name}
-            >
+          <li
+          className={`mx-1 ${
+            currentCategory.name === category.name && !contactSelected && `navActive`
+            }`}
+          key={category.name}
+        >
               <span
                 onClick={() => {
-                  setCurrentCategory(category)
+                  setCurrentCategory(category);
+                  setContactSelected(false);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
